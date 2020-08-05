90bfb6205cbf5ba28dac94d885395f4b2d1dc4f3
grebenuks <grebenuks2406@gmail.com>
2020-08-06 00:29:01 +0300

    Делает основные задания

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import galleryItem from "./gallery-items.js";
 
-let gallery = document.querySelector(".gallery");
+const refs = {
+  gallery: document.querySelector(".gallery"),
+  ulGallery: document.querySelector(".js-gallery"),
+  modalBox: document.querySelector(".js-lightbox"),
+  closeModalBtn: document.querySelector(".lightbox__button"),
+  lightboxImage: document.querySelector(".lightbox__image"),
+  backdrop: document.querySelector(".lightbox__content"),
+};
 
 galleryItem.map((el) => {
-  gallery.insertAdjacentHTML(
+  refs.gallery.insertAdjacentHTML(
     "beforeend",
     `<li class="gallery__item"><a class="gallery__link" href=${el.original}><img class="gallery__image" src=${el.preview} data-source=${el.original} alt=${el.description}/></a></li>`
   );
 });
 
-const ulGallery = document.querySelector(".js-gallery");
-ulGallery.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log(event.target);
-});
+refs.ulGallery.addEventListener("click", openModal);
+refs.closeModalBtn.addEventListener("click", closeModal);
+refs.backdrop.addEventListener("click", closeBackdrop);
+
+function openModal(e) {
+  e.preventDefault();
+  refs.modalBox.classList.add("is-open");
+
+  refs.lightboxImage.src = e.target.getAttribute("data-source");
+}
+
+function closeModal() {
+  refs.modalBox.classList.remove("is-open");
+  refs.lightboxImage.src = "";
+}
+
+function closeBackdrop() {
+  refs.modalBox.classList.remove("is-open");
+  refs.lightboxImage.src = "";
+}
