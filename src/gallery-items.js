5f62a7394c69cb46e29eb3c8dc1e11632525e4e2
grebenuks <grebenuks2406@gmail.com>
2020-08-06 16:10:56 +0300

    Исправляет битую картинку

--- src/gallery-items.js
+++ src/gallery-items.js
@@ -1,10 +1,10 @@
 export default [
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825__340.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
     original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/hokkaido-4202825_1280.jpg",
-    description: "Hokkaido Flower",
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
   },
   {
     preview:
