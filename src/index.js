77daf4fa0f7becd2e9de2aa67c424a306d5f8130
grebenuks <grebenuks2406@gmail.com>
2020-08-07 17:04:42 +0300

    Исправляет добавление галереи

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import galleryItem from "./gallery-items.js";
 
 const refs = {
-  gallery: document.querySelector(".gallery"),
   ulGallery: document.querySelector(".js-gallery"),
   modalBox: document.querySelector(".js-lightbox"),
   closeModalBtn: document.querySelector(".lightbox__button"),
@@ -9,12 +8,14 @@ const refs = {
   backdrop: document.querySelector(".lightbox__content"),
 };
 
-galleryItem.map((el) => {
-  refs.gallery.insertAdjacentHTML(
-    "beforeend",
-    `<li class="gallery__item"><a class="gallery__link" href=${el.original}><img class="gallery__image" src=${el.preview} data-source=${el.original} alt=${el.description}/></a></li>`
-  );
-});
+const gallery = galleryItem
+  .map(
+    (el) =>
+      `<li class="gallery__item"><a class="gallery__link" href=${el.original}><img class="gallery__image" src=${el.preview} data-source=${el.original} alt=${el.description}/></a></li>`
+  )
+  .join("");
+
+refs.ulGallery.insertAdjacentHTML("beforeend", gallery);
 
 refs.ulGallery.addEventListener("click", openModal);
 refs.closeModalBtn.addEventListener("click", closeModal);
